ee3b579c4c1a2e48b27ebb0f8092ef2a5f14b4db
nithishkk <109412506+nithishkk@users.noreply.github.com>
2023-02-14 20:02:44 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-h5avka
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-h5avka)
---
+++ src/App.js
@@ -0,0 +1,35 @@
+import React, { useEffect, useState }  from "react";
+
+function DynamicApiOPtion() {
+    const [values,setValues]=useState([])
+    const [options,setOptions]=useState()
+    const [email, setEmail] = useState("");
+
+    useEffect(()=>{
+        fetch("https://jsonplaceholder.typicode.com/users").then((data)=>data.json()).then((val)=>setValues(val))
+    },[])
+
+    console.log(values,"values")
+    return(
+        <div>
+            <select onChange={(e)=>setOptions(e.target.value)}>
+                {
+                    values.map((opts,i)=><option key={i}>{opts.name}</option>)
+                }
+            </select>
+             { options ? <input
+
+autoFocus
+
+type="email"
+
+value={email}
+
+onChange={(e) => setEmail(e.target.value)}
+
+/>:""} 
+        </div>
+    );
+}
+
+export default DynamicApiOPtion;
